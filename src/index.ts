125318ec4fa65b7713a09caf905047ebe413a5f5
hrz6976 <rzhe@pku.edu.cn>
2025-03-19 19:50:36 +0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,63 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import git from 'isomorphic-git';
+import http from 'isomorphic-git/http/web';
+
+type Ref = {
+	ref: string;
+	oid: string;
+};
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const gitUrls = url.searchParams.getAll('url');
+		const prefix = url.searchParams.get('prefix') || 'refs/tags/';
+		const AllRefs: Record<string, Ref[]> = {};
+		const AllErrs: Record<string, string> = {};
+		if (gitUrls.length === 0) {
+			return new Response(JSON.stringify({ error: 'Missing required parameter "url"' }), {
+				status: 400,
+				headers: { 'Content-Type': 'application/json' },
+			});
+		}
+		if (gitUrls.length >= 10) {
+			return new Response(JSON.stringify({ error: 'Too many URLs provided. Maximum is 10.' }), {
+				status: 400,
+				headers: { 'Content-Type': 'application/json' },
+			});
+		}
+		for (const gitUrl of gitUrls) {
+			try {
+				if (!gitUrl || !prefix) {
+					return new Response(
+						JSON.stringify({
+							error: 'Missing required parameters. Both "url" and "prefix" are required.',
+						}),
+						{
+							status: 500,
+							headers: { 'Content-Type': 'application/json' },
+						}
+					);
+				}
+				const refs = await git.listServerRefs({
+					http,
+					url: gitUrl,
+					prefix: prefix,
+					peelTags: true,
+				});
+				AllRefs[gitUrl] = refs;
+			} catch (error) {
+				if (error instanceof Error) {
+					AllErrs[gitUrl] = error.message;
+				} else {
+					AllErrs[gitUrl] = 'An unexpected error occurred';
+				}
+			}
+		}
+		return new Response(JSON.stringify({ data: AllRefs, errors: AllErrs }), {
+			headers: { 'Content-Type': 'application/json' },
+		});
 	},
 } satisfies ExportedHandler<Env>;
